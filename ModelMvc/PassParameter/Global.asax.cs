0a98a30cfa747509749d532ac5ac66431e6d73e6
Neli Todorova <32536500+nelci592@users.noreply.github.com>
2019-09-23 16:59:34 +0300

    How to set the report source and parameters through a model in ASP .NET MVC app

---
+++ ModelMvc/PassParameter/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace PassParameter
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Telerik.Reporting.Services.WebApi.ReportsControllerConfiguration.RegisterRoutes(System.Web.Http.GlobalConfiguration.Configuration);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ ModelMvc/PassParameter/Models/ReportModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PassParameter.Models
+{
+    public class ReportModel
+    {
+        public string ReportName { get; set; }
+        public Dictionary<string,object> Parameters { get; set; }
+
+    }
+}
