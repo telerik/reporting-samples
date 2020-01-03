c7951b3969e065a8fef52b2b5d7d55c901ccc604
Todor Arabadzhiev <todor_ia@abv.bg>
2020-01-03 17:11:19 +0200

    sample for the KB 'How to Put the Report's DocumentName as a Title for the Browser Tab'

---
+++ ReportNameAsWebPageTitle/WebFormsApp/Startup.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebFormsApp.Startup))]
+namespace WebFormsApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+        }
+    }
+}
