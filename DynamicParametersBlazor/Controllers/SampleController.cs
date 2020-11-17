b7eb901c2b0dc7be004d3165cd094c993b1d1c0b
Dimitar Nikolov <dimitarnikolovv06@gmail.com>
2020-11-17 12:47:41 +0200

    Blazor Project which demonstrates how to set the parameters of the report dynamically

---
+++ DynamicParametersBlazor/Controllers/SampleController.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp.BlazorHtml5Demo.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+    }
+}
