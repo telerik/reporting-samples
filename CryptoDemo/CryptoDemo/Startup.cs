05090e466a247e152140c0ec7adc76941a5c5dcb
Neli Todorova <32536500+nelci592@users.noreply.github.com>
2021-05-17 23:40:01 +0300

    Added resource folder

--- CryptoDemo/CryptoDemo/Startup.cs
+++ CryptoDemo/CryptoDemo/Startup.cs
@@ -1,60 +1,69 @@
-using Telerik.WebReportDesigner.Services;
-using Telerik.Reporting.Cache.File;
-using Telerik.Reporting.Services;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using System.IO;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace CryptoDemo
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.DependencyInjection.Extensions;
+    using Microsoft.Extensions.Hosting;
+    using System;
+    using Telerik.Reporting.Cache.File;
+    using Telerik.Reporting.Services;
+    using Telerik.WebReportDesigner.Services;
+
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
         {
-            Configuration = configuration;
+            this.Configuration = configuration;
+            this.WebHostEnvironment = webHostEnvironment;
         }
 
         public IConfiguration Configuration { get; }
 
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddMvc();
+            services.AddRazorPages()
+                .AddNewtonsoftJson();
 
-            services.TryAddSingleton((Func<IServiceProvider, IReportServiceConfiguration>)(sp =>
+            var reportsPath = System.IO.Path.Combine(this.WebHostEnvironment.ContentRootPath, "Reports");
+
+            // Configure dependencies for ReportsController.
+            services.TryAddSingleton<IReportServiceConfiguration>(sp =>
                 new ReportServiceConfiguration
                 {
+                    // The default ReportingEngineConfiguration will be initialized from appsettings.json or appsettings.{EnvironmentName}.json:
                     ReportingEngineConfiguration = sp.GetService<IConfiguration>(),
-                    HostAppId = "WebReportDesignerApp",
-                    Storage = new FileStorage(),
-                    ReportSourceResolver = new UriReportSourceResolver(GetReportsDir(sp))
-                }));
 
+                    // In case the ReportingEngineConfiguration needs to be loaded from a specific configuration file, use the approach below:
+                    // ReportingEngineConfiguration = ResolveSpecificReportingConfiguration(WebHostEnvironment),
+                    HostAppId = "ReportingNet5",
+                    Storage = new FileStorage(),
+                    ReportSourceResolver = new TypeReportSourceResolver()
+                        .AddFallbackResolver(new UriReportSourceResolver(reportsPath))
+                });
+
+            // Configure dependencies for ReportDesignerController.
             services.TryAddSingleton<IReportDesignerServiceConfiguration>(sp => new ReportDesignerServiceConfiguration
             {
                 DefinitionStorage = new FileDefinitionStorage(
-                    GetReportsDir(sp)),
+                    reportsPath),
                 SettingsStorage = new FileSettingsStorage(
-                    Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Telerik Reporting")),
+                    System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Telerik Reporting")),
                 ResourceStorage = new ResourceStorage(
-                    Path.Combine(GetReportsDir(sp), "Resources"))
+                    System.IO.Path.Combine(reportsPath,"..","Resources"))
             });
-            services.AddRazorPages().AddNewtonsoftJson();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
+            if (this.WebHostEnvironment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
@@ -64,21 +73,25 @@ namespace CryptoDemo
             }
 
             app.UseStaticFiles();
-
             app.UseRouting();
-
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
-              endpoints.MapControllers();
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
 
-        static string GetReportsDir(IServiceProvider sp)
+        /// <summary>
+        /// Loads a reporting configuration from a specific JSON-based configuration file.
+        /// </summary>
+        /// <param name="environment">The current web hosting environment used to obtain the content root path</param>
+        /// <returns>IConfiguration instance used to initialize the Reporting engine</returns>
+        static IConfiguration ResolveSpecificReportingConfiguration(IWebHostEnvironment environment)
         {
-            return Path.Combine(sp.GetService<IWebHostEnvironment>().ContentRootPath, "Reports");
+            // If a specific configuration needs to be passed to the reporting engine, add it through a new IConfiguration instance.
+            var reportingConfigFileName = System.IO.Path.Combine(environment.ContentRootPath, "reportingAppSettings.json");
+            return new ConfigurationBuilder()
+                .AddJsonFile(reportingConfigFileName, true)
+                .Build();
         }
     }
 }
