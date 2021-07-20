24ec6771928fcc678a0b649c254948f0d7df8df6
Neli Todorova <32536500+nelci592@users.noreply.github.com>
2021-07-20 17:56:58 +0300

    First steps demo project

---
+++ FirstStepsProject/FirstStepsProject/Controllers/ReportsController.cs
@@ -0,0 +1,45 @@
+namespace FirstStepsProject.Controllers
+{
+    using System.IO;
+    using System.Web;
+    using Telerik.Reporting.Cache.File;
+    using Telerik.Reporting.Services;
+    using Telerik.Reporting.Services.WebApi;
+
+	//The class name determines the service URL. 
+	//ReportsController class name defines /api/reports/ service URL.
+    public class ReportsController : ReportsControllerBase
+    {
+        static ReportServiceConfiguration configurationInstance;
+
+        static ReportsController()
+        {
+            //This is the folder that contains the report definitions
+            //In this case this is the Reports folder
+            var appPath = HttpContext.Current.Server.MapPath("~/");
+            var reportsPath = Path.Combine(appPath, "Reports");
+
+            //Add resolver for trdx/trdp report definitions, 
+            //then add resolver for class report definitions as fallback resolver; 
+            //finally create the resolver and use it in the ReportServiceConfiguration instance.
+            var resolver = new UriReportSourceResolver(reportsPath)
+                .AddFallbackResolver(new TypeReportSourceResolver());
+
+            //Setup the ReportServiceConfiguration
+            configurationInstance = new ReportServiceConfiguration
+                {
+                    HostAppId = "Html5App",
+                    Storage = new FileStorage(),
+                    ReportSourceResolver = resolver,
+                    // ReportSharingTimeout = 0,
+                    // ClientSessionTimeout = 15,
+                };
+        }
+
+        public ReportsController()
+        {
+			//Initialize the service configuration
+            this.ReportServiceConfiguration = configurationInstance;
+        }
+    }
+}
