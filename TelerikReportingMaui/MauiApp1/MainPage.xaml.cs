2353b1aa606ae9d65937d880fa44c5ca600f4250
Neli Todorova <32536500+nelci592@users.noreply.github.com>
2022-09-01 10:10:28 +0300

    Telerik Reporting in .NET Maui and .NET Maui Blazor app

---
+++ TelerikReportingMaui/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+   
+        //this.webView.Source = new HtmlWebViewSource
+        //{
+        //    Html = @"<HTML><BODY><H1>.NET MAUI</H1><p><a href=" + "viewer.html" + ">next page</a></p></BODY></HTML>"
+        //};
+
+
+    }
+
+}
---
+++ TelerikReportingMaui/MauiApp1/MauiProgram.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiApp1;
+using Telerik.Maui.Controls.Compatibility;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+            .UseTelerik()
+            .UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		return builder.Build();
+	}
+}
