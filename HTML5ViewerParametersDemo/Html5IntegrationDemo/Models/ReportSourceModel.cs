16b8b78e63597916bdaa86518173e6c864f8af34
Todor Arabadzhiev <arabadzhiev@progress.com>
2022-12-21 17:36:46 +0200

    Add files via upload

---
+++ HTML5ViewerParametersDemo/Html5IntegrationDemo/Models/ReportSourceModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace HTML5ViewerParametersDemo.Models
+{
+    public class ReportSourceModel
+    {
+        public string Report { get; set; }
+        public Dictionary<string, object> Parameters { get; set; }
+    }
+}
