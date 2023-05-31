58dd75fa11f5b8dccdb541cf1370286d2683d02b
Dimitar Nikolov <dimitarnikolovv06@gmail.com>
2023-05-31 15:43:46 +0300

    add sample vue3 application with viewer and designer implementation

---
+++ reporting-vue/src/components/ReportViewer.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="hello">
+      <h1></h1>
+      <div id="reportViewer1">loading...</div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ReportViewer',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    mounted () {
+      this.$nextTick(function () {
+        $('#reportViewer1')
+          .telerik_ReportViewer({
+            serviceUrl: 'http://demos.telerik.com/reporting/api/reports/',
+            reportSource: {
+              report: 'Dashboard.trdx'
+            },
+            viewMode: telerikReportViewer.ViewModes.INTERACTIVE,
+            scaleMode: telerikReportViewer.ScaleModes.SPECIFIC,
+            scale: 1.0,
+            sendEmail: { enabled: true }
+          })
+      })
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+    body {
+        font-family: Verdana, Arial;
+        margin: 5px;
+    }
+  
+    #reportViewer1 {
+        position: relative;
+        width: 80vw;
+        height: 1000px;
+    }
+  </style>
---
+++ reporting-vue/src/views/ReportViewerView.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import ReportViewer from '../components/ReportViewer.vue';
+</script>
+
+
+<template>
+    <ReportViewer />
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
