507bb098f4d90c28701864dda1d4e66ac0be1506
Todor Arabadzhiev <arabadzhiev@progress.com>
2023-09-07 15:09:54 +0300

    add MauiBlazorPdfReporting projects

---
+++ MauiBlazorPdfReporting/MauiReporting/MauiBlazorViewer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiBlazorViewer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
