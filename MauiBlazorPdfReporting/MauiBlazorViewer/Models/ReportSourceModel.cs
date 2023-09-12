f5b0f6a30471ba4fba2b1d7fcf2eaf8da4eadd16
Todor Arabadzhiev <arabadzhiev@progress.com>
2023-09-12 17:57:35 +0300

    Maui PDF sample updated to use online demos

---
+++ MauiBlazorPdfReporting/MauiBlazorViewer/Models/ReportSourceModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBlazorViewer.Models
+{
+    public class ReportSourceModel
+    {
+        [System.Text.Json.Serialization.JsonPropertyName("Report")]
+        public string Report { get; set; }
+        [System.Text.Json.Serialization.JsonPropertyName("ParameterValues")]
+        public IDictionary<string, object> ParameterValues { get; set; }
+    }
+}
