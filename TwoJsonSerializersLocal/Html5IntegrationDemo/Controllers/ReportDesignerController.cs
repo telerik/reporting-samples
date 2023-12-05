d4004c3fccbc18aaa37c9897b4d07059da2f5b0c
Nikolay Yakov <yakov@progress.com>
2023-12-05 13:58:34 +0200

    chore: added a sample that show how to configure a local solution for two json serializers

---
+++ TwoJsonSerializersLocal/Html5IntegrationDemo/Controllers/ReportDesignerController.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharp.Net8.Html5IntegrationDemo.Controllers {
+	using Microsoft.AspNetCore.Mvc;
+	using Telerik.Reporting.Services;
+	using Telerik.WebReportDesigner.Services;
+	using Telerik.WebReportDesigner.Services.Controllers;
+
+	[Route("api/reportdesigner")]
+    public class ReportDesignerController : ReportDesignerControllerSTJBase {
+        public ReportDesignerController(IReportDesignerServiceConfiguration reportDesignerServiceConfiguration, IReportServiceConfiguration reportServiceConfiguration)
+            : base(reportDesignerServiceConfiguration, reportServiceConfiguration)
+        {
+        }
+    }
+}
