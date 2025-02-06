5dbba4abe867a495ae59f9be653c56356540f89c
Dimitar Nikolov <dnikolov@progress.com>
2025-02-06 11:19:26 +0200

    create localization example for NBRV in WebAssembly Standalone app

---
+++ NativeBlazorRvLocalizationWebAssembly/NativeBlazorRvLocalizationWebAssembly/Services/ReportViewerStringLocalizer.cs
@@ -0,0 +1,16 @@
+﻿using NativeBlazorRvLocalizationWebAssembly.Resources;
+using Telerik.ReportViewer.BlazorNative.Services;
+
+namespace NativeBlazorRvLocalizationWebAssembly.Services
+{
+    public class ReportViewerStringLocalizer : ITelerikReportingStringLocalizer
+    {
+        public string this[string key]
+        {
+            get
+            {                 
+                return ReportViewerMessages.ResourceManager.GetString(key, ReportViewerMessages.Culture) ?? key;
+            }
+        }
+    }
+}
---
+++ NativeBlazorRvLocalizationWebAssembly/NativeBlazorRvLocalizationWebAssembly/Services/TelerikBlazorUiStringLocalizerc.cs
@@ -0,0 +1,16 @@
+﻿using Telerik.Blazor.Resources;
+using Telerik.Blazor.Services;
+
+namespace NativeBlazorRvLocalizationWebAssembly.Services
+{
+    public class TelerikBlazorUiStringLocalizer : ITelerikStringLocalizer
+    {
+        public string this[string key]
+        {
+            get
+            {
+                return Messages.ResourceManager.GetString(key, Messages.Culture) ?? key;
+            }
+        }
+    }
+}
